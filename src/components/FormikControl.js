7d6a337dbf338d8966bf12054b9b80d7f99bddce
Alpana Kushwaha <alpana95kush@gmail.com>
2023-08-28 23:59:21 +0530

    Select Component

--- src/components/FormikControl.js
+++ src/components/FormikControl.js
@@ -2,6 +2,18 @@ import React from "react";
 import Input from "./Input";
 import TextArea from "./TextArea";
 function FormikControl(props) {
+  const dropdownOptions = [
+    { key: "Select an option", value: "" },
+    { key: "Option 1", value: "option1" },
+    { key: "Option 2", value: "option2" },
+    { key: "Option 3", value: "option3" },
+  ];
+
+const initialValues=(
+  email:'',
+  description:''
+)
+
   const { control, ...rest } = props;
   switch (control) {
     case "input":
@@ -9,6 +21,7 @@ function FormikControl(props) {
     case "textarea":
       return <TextArea {...rest} />;
     case "select":
+      return <Select {...rest} />;
     case "radio":
     case "checkbox":
     case "date":
