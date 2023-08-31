4d11d67fa4ad1571a06b46c68cdd9a87a2f05301
Alpana Kushwaha <alpana95kush@gmail.com>
2023-09-01 00:25:46 +0530

    RadioButtons Component incomplete

---
+++ src/components/RadioButtons.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const RadioButtons = () => {
+  const { label, name, options, ...rest } = props;
+  return (
+    <div classsName="form-control">
+      <label>{label}</label>
+      <Field name={name} {...rest}>
+        {({ field }) => {
+          return options.map((option) => {
+            return (
+              <React.Fragment key={option.key}>
+                <input
+                  type="radio"
+                  id={option.value}
+                  {...field}
+                  value={option.value}
+                  checked={field.value === option.value}
+                />
+                <label htmlFor={option.value}>{option.key}</label>
+              </React.Fragment>
+            );
+          });
+        }}
+      </Field>
+        <ErrorMessage name={name} component={TextError}/>
+
+    </div>
+  );
+};
+
+export default RadioButtons;
